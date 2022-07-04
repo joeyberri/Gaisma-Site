b73b13ff494482f32220e5fe950c3af4b43c918c
Joey <joeyberri@gmail.com>
2022-07-04 17:09:17 +0000

    added the audition module

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Thank you for contacting us. We will be in touch with you very soon '
 	);
 
     $name = @trim(stripslashes($_POST['name'])); 
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'joeyberri@gmail.com';//replace with your email
+    $email_to = 'gaismainc@gmail.com';//replace with your email
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
