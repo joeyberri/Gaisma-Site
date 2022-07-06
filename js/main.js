8314d60c510d7971e97828d7dcc8f1b936dc2ae4
Joey <joeyberri@gmail.com>
2022-07-06 20:43:34 +0000

    Added smooth scrolling for # href tags

--- js/main.js
+++ js/main.js
@@ -12,6 +12,15 @@ jQuery(function ($) {
       }
     );
 
+  $("a[href*=#]").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      500
+    );
+    return false;
+  });
   //Scroll Menu
 
   function menuToggle() {
