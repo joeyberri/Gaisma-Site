7d57e5f67e20c1cdbd7c3d55250041bc9545b6c8
Joey <joeyberri@gmail.com>
2022-07-06 15:53:36 +0000

    work on views

--- js/main.js
+++ js/main.js
@@ -1,150 +1,131 @@
-jQuery(function($) {'use strict',
-	
-	//Countdown js
-	 $("#countdown").countdown({
-			date: "10 july 2014 12:00:00",
-			format: "on"
-		},
-		
-		function() {
-			// callback function
-		});
-	
+jQuery(function ($) {
+  "use strict",
+    //Countdown js
+    $("#countdown").countdown(
+      {
+        date: "01 august 2022 05:00:00",
+        format: "on",
+      },
 
-	
-	//Scroll Menu
+      function () {
+        // callback function
+      }
+    );
 
-	function menuToggle()
-	{
-		var windowWidth = $(window).width();
+  //Scroll Menu
 
-		if(windowWidth > 767 ){
-			$(window).on('scroll', function(){
-				if( $(window).scrollTop()>405 ){
-					$('.main-nav').addClass('fixed-menu animated slideInDown');
-				} else {
-					$('.main-nav').removeClass('fixed-menu animated slideInDown');
-				}
-			});
-		}else{
-			
-			$('.main-nav').addClass('fixed-menu animated slideInDown');
-				
-		}
-	}
+  function menuToggle() {
+    var windowWidth = $(window).width();
 
-	menuToggle();
-	
-	
-	// Carousel Auto Slide Off
-	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
-		interval: false
-	});
+    if (windowWidth > 767) {
+      $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 405) {
+          $(".main-nav").addClass("fixed-menu animated slideInDown");
+        } else {
+          $(".main-nav").removeClass("fixed-menu animated slideInDown");
+        }
+      });
+    } else {
+      $(".main-nav").addClass("fixed-menu animated slideInDown");
+    }
+  }
 
+  menuToggle();
 
-	// Contact form validation
-	var form = $('.contact-form');
-	form.submit(function () {'use strict',
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
+  // Carousel Auto Slide Off
+  $("#event-carousel, #twitter-feed, #sponsor-carousel ").carousel({
+    interval: false,
+  });
 
-	$( window ).resize(function() {
-		menuToggle();
-	});
+  // Contact form validation
+  var form = $(".contact-form");
+  form.submit(function () {
+    "use strict", ($this = $(this));
+    $.post(
+      $(this).attr("action"),
+      function (data) {
+        $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+      },
+      "json"
+    );
+    return false;
+  });
 
-	$('.main-nav ul').onePageNav({
-		currentClass: 'active',
-	    changeHash: false,
-	    scrollSpeed: 900,
-	    scrollOffset: 0,
-	    scrollThreshold: 0.3,
-	    filter: ':not(.no-scroll)'
-	});
+  $(window).resize(function () {
+    menuToggle();
+  });
 
+  $(".main-nav ul").onePageNav({
+    currentClass: "active",
+    changeHash: false,
+    scrollSpeed: 900,
+    scrollOffset: 0,
+    scrollThreshold: 0.3,
+    filter: ":not(.no-scroll)",
+  });
 });
 
-
 // Google Map Customization
-(function(){
+(function () {
+  var map;
 
-	var map;
+  map = new GMaps({
+    el: "#gmap",
+    lat: 43.04446,
+    lng: -76.130791,
+    scrollwheel: false,
+    zoom: 16,
+    zoomControl: false,
+    panControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    overviewMapControl: false,
+    clickable: false,
+  });
 
-	map = new GMaps({
-		el: '#gmap',
-		lat: 43.04446,
-		lng: -76.130791,
-		scrollwheel:false,
-		zoom: 16,
-		zoomControl : false,
-		panControl : false,
-		streetViewControl : false,
-		mapTypeControl: false,
-		overviewMapControl: false,
-		clickable: false
-	});
+  var image = "images/map-icon.png";
+  map.addMarker({
+    lat: 43.04446,
+    lng: -76.130791,
+    icon: image,
+    animation: google.maps.Animation.DROP,
+    verticalAlign: "bottom",
+    horizontalAlign: "center",
+    backgroundColor: "#3e8bff",
+  });
 
-	var image = 'images/map-icon.png';
-	map.addMarker({
-		lat: 43.04446,
-		lng: -76.130791,
-		icon: image,
-		animation: google.maps.Animation.DROP,
-		verticalAlign: 'bottom',
-		horizontalAlign: 'center',
-		backgroundColor: '#3e8bff',
-	});
+  var styles = [
+    {
+      featureType: "road",
+      stylers: [{ color: "#b4b4b4" }],
+    },
+    {
+      featureType: "water",
+      stylers: [{ color: "#d8d8d8" }],
+    },
+    {
+      featureType: "landscape",
+      stylers: [{ color: "#f1f1f1" }],
+    },
+    {
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#000000" }],
+    },
+    {
+      featureType: "poi",
+      stylers: [{ color: "#d9d9d9" }],
+    },
+    {
+      elementType: "labels.text",
+      stylers: [{ saturation: 1 }, { weight: 0.1 }, { color: "#000000" }],
+    },
+  ];
 
+  map.addStyle({
+    styledMapName: "Styled Map",
+    styles: styles,
+    mapTypeId: "map_style",
+  });
 
-	var styles = [ 
-
-	{
-		"featureType": "road",
-		"stylers": [
-		{ "color": "#b4b4b4" }
-		]
-	},{
-		"featureType": "water",
-		"stylers": [
-		{ "color": "#d8d8d8" }
-		]
-	},{
-		"featureType": "landscape",
-		"stylers": [
-		{ "color": "#f1f1f1" }
-		]
-	},{
-		"elementType": "labels.text.fill",
-		"stylers": [
-		{ "color": "#000000" }
-		]
-	},{
-		"featureType": "poi",
-		"stylers": [
-		{ "color": "#d9d9d9" }
-		]
-	},{
-		"elementType": "labels.text",
-		"stylers": [
-		{ "saturation": 1 },
-		{ "weight": 0.1 },
-		{ "color": "#000000" }
-		]
-	}
-
-	];
-
-	map.addStyle({
-		styledMapName:"Styled Map",
-		styles: styles,
-		mapTypeId: "map_style"  
-	});
-
-	map.setStyle("map_style");
-}());
-
-
-
+  map.setStyle("map_style");
+})();
